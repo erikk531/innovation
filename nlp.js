a5536631dc1bc96e9b8feeba13164dfa3528cb13
Erik Koebke <erikkoebke@Eriks-MacBook-Pro.local>
2017-01-24 10:41:15 -0500

    updated

--- nlp.js
+++ nlp.js
@@ -9,38 +9,38 @@ var witClient = new Wit({accessToken: config.witAuthToken});
 var isMissingValues = false;
 
 module.exports.generateBotResponse = function(incomingMessage, messageFrom) {
-    console.log("generateBotResponse called");
     var deferred = q.defer();
 
     // get the session id based on the phone number that is texting bot
-    getSessionIdAndContext(messageFrom).then(function(sessionIdAndContext) {
-        var sessionId = sessionIdAndContext.sessionId;
-        var context = sessionIdAndContext.context;
+    getSessionInfo(messageFrom).then(function(sessionInfo) {
+        var sessionId = sessionInfo.sessionId;
+        var context = sessionInfo.context;
+        var missingInfo = sessionInfo.missingInfo;
+        var orderInfo = sessionInfo.orderInfo;
 
         witClient.message(incomingMessage).then((data) => {
             var messageDetails = JSON.stringify(data);
             var jsonMessageDetails = JSON.parse(messageDetails);
             var entities = jsonMessageDetails.entities;
 
-            if (context == "initialMessage") {
-                console.log("checkOrderInfo");
+            console.log(jsonMessageDetails);
 
-                var whatTheOrderIsMissing = checkOrderInfo(entities);
-
-                // the order is not missing any items
-                if (whatTheOrderIsMissing == null) {
-                    setContext({'orderTime': {'orderTime': 2}}, messageFrom).then(function(updatedContext) {
-                        deferred.resolve(module.exports.generateBotResponse(incomingMessage, messageFrom));
-                    }, function(err) {
-                        console.log(err);
+            // no order entities found
+            if (_.isEmpty(entities)) {
+                deferred.resolve("That doesn't appear to be on our menu.");
+            } else {
+                if (context == "lookingForOrder") {
+                    checkOrderInfo(entities, messageFrom, missingInfo, orderInfo).then(function(whatTheOrderIsMissing) {
+                        deferred.resolve(whatTheOrderIsMissing);
+                    }, function(nothingMissing) {
+                        setContext("orderObtained", messageFrom).then(function(updatedContext) {
+                            deferred.resolve("Thank you very much! Your order will be ready in 5 minutes. I'll let you know when it is ready.");
+                        }, function(error) {
+                            console.log(error);
+                        });
                     });
-                } else {
-                    setContext({'whatTheOrderIsMissing': whatTheOrderIsMissing}, messageFrom).then(function(updatedContext) {
-                        deferred.resolve(module.exports.generateBotResponse(incomingMessage, messageFrom));
-                    }, function(err) {
-                        console.log(err);
-                    });
-
+                } else if (context == "orderObtained") {
+                    deferred.resolve("Your order will be over shortly.");
                 }
             }
         }).catch(console.error);
@@ -51,83 +51,162 @@ module.exports.generateBotResponse = function(incomingMessage, messageFrom) {
     return deferred.promise;
 }
 
-function checkOrderInfo(entities) {
+// promise
+function checkOrderInfo(entities, messageFrom, missingInfo, orderInfo) {
+    var deferred = q.defer();
     var isMissingValues = false;
-    var theOrder = {};
-    theOrder.menuItems = {};
 
-    if (entities.drink_type != null) {
-        // array of drink types being ordered
-        var drinkTypes = getWitEntityValues(entities.drink_type);
+    if (_.isEmpty(missingInfo) && _.isEmpty(orderInfo)) {
+        console.log("Must be first text with order information");
+        var theOrder = {};
+        theOrder.menuItems = {};
 
-        // build the order JSON
-        _.forEach(drinkTypes, function(drinkType) {
-            theOrder.menuItems[drinkType] = {"size": "", "quantity": "", "additions": []};
-        });
+        if (entities.drink_type != null) {
+            // array of drink types being ordered
+            var drinkTypes = getWitEntityValues(entities.drink_type);
 
-        // populate each drink order with the additional details the user gives
-        _.forIn(entities, function(entityValues, entityType) {
-            if (entityType != "drink_type") {
-                var values = getWitEntityValues(entityValues);
-                _.forEach(values, function(value) {
-                    if (isNumeric(value)) {
-                        theOrder.menuItems[entityType].quantity = value;
-                    } else if (value == "small" || value == "medium" || value == "large") {
-                        theOrder.menuItems[entityType].size = value;
-                    } else {
-                        theOrder.menuItems[entityType].additions.push(value);
-                    }
-                });
-            }
-        });
+            // build the order JSON
+            _.forEach(drinkTypes, function(drinkType) {
+                theOrder.menuItems[drinkType] = {"size": "", "quantity": "", "additions": []};
+            });
 
-        var missingValues = {};
-
-        // iterate the drinks being ordered - take note of which values are missing for each drink
-        _.forEach(drinkTypes, function(drinkType) {
-            if (theOrder.menuItems[drinkType].size == "" || theOrder.menuItems[drinkType].quantity == "") {
-                isMissingValues = true;
-                missingValues[drinkType] = theOrder.menuItems[drinkType];
-            }
-        });
-
-        if (isMissingValues) {
-            var askForWhatIsMissing = "Looks like I need a little bit more info from you to complete your order."
-
-            _.forIn(missingValues, function(values, drink) {
-                // update drink if iced_coffee
-                if (drink == "iced_coffee") {
-                    drink = "iced coffee";
-                }
-
-                // no size or quantity specified
-                if (values.size == "" && values.quantity == "") {
-                    askForWhatIsMissing = askForWhatIsMissing.concat("What size (small, medium, or large) and how many "+drink+"'s did you want?");
-                } else {
-
-
-                    if (values.size == "") {
-                        if (values.quantity > 1) {
-                            askForWhatIsMissing = askForWhatIsMissing.concat(" What size (small, medium, or large) "+drink+"'s did you want?");
+            // populate each drink order with the additional details the user gives
+            _.forIn(entities, function(entityValues, entityType) {
+                if (entityType != "drink_type") {
+                    var values = getWitEntityValues(entityValues);
+                    _.forEach(values, function(value) {
+                        if (isNumeric(value)) {
+                            theOrder.menuItems[entityType].quantity = value;
+                        } else if (value == "small" || value == "medium" || value == "large") {
+                            theOrder.menuItems[entityType].size = value;
                         } else {
-                            askForWhatIsMissing = askForWhatIsMissing.concat(" What size (small, medium, or large) "+drink+" did you want?");
+                            theOrder.menuItems[entityType].additions.push(value);
                         }
-                    }
-
-                    if (values.quantity == "") {
-                        askForWhatIsMissing = askForWhatIsMissing.concat(" How many "+drink+"'s did you want?");
-                    }
+                    });
                 }
             });
 
-            return askForWhatIsMissing;
+            var missingValues = {};
+            setOrderInfo(theOrder, messageFrom).then(function(orderInfo) {
+                // iterate the drinks being ordered - take note of which values are missing for each drink
+                _.forEach(drinkTypes, function(drinkType) {
+                    console.log("iterate");
+                    if (theOrder.menuItems[drinkType].size == "" || theOrder.menuItems[drinkType].quantity == "") {
+                        console.log("it is true");
+                        isMissingValues = true;
+                        missingValues[drinkType] = theOrder.menuItems[drinkType];
+                    }
+                });
+
+                if (isMissingValues == true) {
+                    // set missing values
+                    // generate response
+                    console.log("values are missing");
+
+                    setMissingInfo(missingValues, messageFrom).then(function(missingInfo) {
+
+                        var resp = generateMissingInfoResponse(missingValues);
+                        deferred.resolve(resp);
+
+                    }, function(error) {
+                        console.log("Error setting missing info");
+                    });
+                } else {
+                    console.log("nothing is missing");
+                    deferred.reject("Nothing is missing.");
+                }
+            }, function(error) {
+                console.log("error setting order info");
+            });
+
         }
-        // nothing is missing from the order
-        else {
-            return null;
-        }
+    } else if (!_.isEmpty(missingInfo) && !_.isEmpty(orderInfo)) {
+        // the user did not provide enough information and we need to reconcile the fields that are missing with the
+        // info that the user re-provides
+
+        var drinkTypes = getWitEntityValues(entities.drink_type);
+
+        // get each drink with a missing value
+        _.forIn(missingInfo, function(values, missingDrink) {
+            // if new input includes this drink
+            if (entities[missingDrink] != null) {
+                // update missingInfo and orderInfo
+                // if all missingInfo filled in - set isMissingInfo to false
+                var updatedValues = entities[missingDrink];
+                for (var i = 0; i < updatedValues.length; i++) {
+                    // i.e. small, 3, large, etc..
+                    var updatedValue = updatedValues[i].value;
+                    if (typeof updatedValue == 'number') {
+                        missingInfo[missingDrink].quantity = updatedValue;
+                        orderInfo.menuItems[missingDrink].quantity = updatedValue;
+                    } else {
+                        missingInfo[missingDrink].size = updatedValue;
+                        orderInfo.menuItems[missingDrink].size = updatedValue;
+                    }
+                }
+
+            }
+        });
+
+        var isMissingInfo = false;
+        _.forIn(missingInfo, function(values, missingDrink) {
+            if (values.size == "" || values.quantity == "") {
+                isMissingInfo = true;
+            }
+        });
+
+        setMissingInfo(missingInfo, messageFrom).then(function(missingInfo) {
+            setOrderInfo(orderInfo, messageFrom).then(function(orderInfo) {
+                if (isMissingInfo) {
+                    var resp = generateMissingInfoResponse(missingInfo);
+                    deferred.resolve(resp);
+                } else {
+                    deferred.reject("Nothing is missing.");
+                }
+            }, function(error) {
+                console.log(error);
+            });
+        }, function(error) {
+            console.log(error);
+        });
+
+
+
+
     }
 
+    return deferred.promise;
+
+}
+
+function generateMissingInfoResponse(missingValues) {
+    var askForWhatIsMissing = "Looks like I need a little bit more info from you to complete your order."
+
+    _.forIn(missingValues, function(values, drink) {
+        // update drink if iced_coffee
+        if (drink == "iced_coffee") {
+            drink = "iced coffee";
+        }
+
+        // no size or quantity specified
+        if (values.size == "" && values.quantity == "") {
+            askForWhatIsMissing = askForWhatIsMissing.concat(" What size (small, medium, or large) and how many "+drink+"'s did you want?");
+        } else {
+            if (values.size == "") {
+                if (values.quantity > 1) {
+                    askForWhatIsMissing = askForWhatIsMissing.concat(" What size (small, medium, or large) "+drink+"'s did you want?");
+                } else {
+                    askForWhatIsMissing = askForWhatIsMissing.concat(" What size (small, medium, or large) "+drink+" did you want?");
+                }
+            }
+
+            if (values.quantity == "") {
+                askForWhatIsMissing = askForWhatIsMissing.concat(" How many "+drink+"'s did you want?");
+            }
+        }
+    });
+
+    return askForWhatIsMissing;
 }
 
 function getWitEntityValues(entityValues) {
@@ -149,7 +228,7 @@ function isNumeric(value) {
 }
 
 // promise
-function getContextForConversation(messageFrom) {
+function getContextForNumber(messageFrom) {
     var deferred = q.defer();
 
     fs.readFile('sessions.json', function(err, content) {
@@ -199,38 +278,64 @@ function setContext(updatedContext, messageFrom) {
     return deferred.promise;
 }
 
-//// promise
-//function endSession(messageFrom) {
-//    var deferred = q.defer();
-//
-//    fs.readFile('sessions.json', function(err, content) {
-//        if (err) {
-//            console.log("endSession: Error reading sessions.json");
-//        } else {
-//            var parseJson = JSON.parse(content);
-//
-//            // the entry exists
-//            if (parseJson[messageFrom]) {
-//                parseJson[messageFrom].sessionId = "";
-//                parseJson[messageFrom].context = {};
-//                fs.writeFile('sessions.json', JSON.stringify(parseJson), function(err) {
-//                    if (err) {
-//                        deferred.reject("Error writing to sessions.json");
-//                    } else {
-//                        deferred.resolve(parseJson[messageFrom]);
-//                    }
-//                });
-//            } else {
-//                deferred.resolve(parseJson[messageFrom]);
-//            }
-//        }
-//    });
-//
-//    return deferred.promise;
-//}
+function setMissingInfo(missingInfo, messageFrom) {
+    var deferred = q.defer();
+    fs.readFile('sessions.json', function(err, content) {
+        if (err) {
+            console.log("setContext: Error reading sessions.json");
+        } else {
+            var parseJson = JSON.parse(content);
+
+            // the entry exists - set updated context
+            if (parseJson[messageFrom]) {
+                console.log("setMissingInfo: "+parseJson[messageFrom]);
+                parseJson[messageFrom].missingInfo = missingInfo;
+                fs.writeFile('sessions.json', JSON.stringify(parseJson), function(err) {
+                    if (err) {
+                        deferred.reject("Error writing to sessions.json");
+                    } else {
+                        deferred.resolve(missingInfo);
+                    }
+                });
+            } else {
+                deferred.reject("setContext: Entry does not exist");
+            }
+        }
+    });
+
+    return deferred.promise;
+}
+
+function setOrderInfo(orderInfo, messageFrom) {
+    var deferred = q.defer();
+    fs.readFile('sessions.json', function(err, content) {
+        if (err) {
+            console.log("setContext: Error reading sessions.json");
+        } else {
+            var parseJson = JSON.parse(content);
+
+            // the entry exists - set updated context
+            if (parseJson[messageFrom]) {
+                parseJson[messageFrom].orderInfo = orderInfo;
+                fs.writeFile('sessions.json', JSON.stringify(parseJson), function(err) {
+                    if (err) {
+                        deferred.reject("Error writing to sessions.json");
+                    } else {
+                        deferred.resolve(orderInfo);
+                    }
+                });
+            } else {
+                deferred.reject("setContext: Entry does not exist");
+            }
+        }
+    });
+
+    return deferred.promise;
+}
+
 
 // promise
-function getSessionIdAndContext(messageFrom) {
+function getSessionInfo(messageFrom) {
     var deferred = q.defer();
 
     fs.readFile('sessions.json', function(err, content) {
@@ -246,11 +351,11 @@ function getSessionIdAndContext(messageFrom) {
                 // have seen number before - new session
                 if (parseJson[messageFrom].sessionId == "") {
                     var sessionId = new Date().toISOString();
-                    parseJson[messageFrom] = {"sessionId": sessionId, "context": "initialMessage"};
-                    sessionIdAndContext = {"sessionId": sessionId, "context": "initialMessage"};
+                    parseJson[messageFrom] = {"sessionId": sessionId, "context": "lookingForOrder", "missingInfo": {}, "orderInfo": {}};
+                    sessionIdAndContext = {"sessionId": sessionId, "context": "lookingForOrder", "missingInfo": {}, "orderInfo": {}};
                 } else {
                     // current session
-                    sessionIdAndContext = {"sessionId": parseJson[messageFrom].sessionId, "context": parseJson[messageFrom].context};
+                    sessionIdAndContext = {"sessionId": parseJson[messageFrom].sessionId, "context": parseJson[messageFrom].context, "missingInfo": parseJson[messageFrom].missingInfo, "orderInfo": parseJson[messageFrom].orderInfo};
 
                 }
 
@@ -261,13 +366,13 @@ function getSessionIdAndContext(messageFrom) {
                 console.log("getSessionId: Entry does not exist.");
 
                 var sessionId = new Date().toISOString();
-                parseJson[messageFrom] = {"sessionId": sessionId, "context": "initialMessage"};
+                parseJson[messageFrom] = {"sessionId": sessionId, "context": "lookingForOrder", "missingInfo": {}, "orderInfo": {}};
                 fs.writeFile('sessions.json', JSON.stringify(parseJson), function(err) {
                     if (err) {
                         deferred.reject("Error writing to sessions.json");
                     }
 
-                    var sessionIdAndContext = {"sessionId": sessionId, "context": "initialMessage"};
+                    var sessionIdAndContext = {"sessionId": sessionId, "context": "lookingForOrder", "missingInfo": {}, "orderInfo": {}};
                     deferred.resolve(sessionIdAndContext);
                 });
             }
